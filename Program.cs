1004bf65fa09fb1cde87fdb79141141380c1f887
amhc082024 <amhc082024@gmail.com>
2024-09-16 11:11:36 +0200

    v0.5

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Oppgave1609;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // En string, det er datatype som inneholder tekst.
+        string helloWorld = "Hello, World";
+        Console.WriteLine(helloWorld);
+
+        // En integer er en datatype som inneholder heltall.
+        int x = 10;
+        Console.WriteLine(x);
+        
+    }
+}
